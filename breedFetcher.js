0c6a8d806b4dcfc6f330f0973af812be5cf736a8
dantan123 <dtan9988@gmail.com>
2021-01-15 01:15:24 +0000

    add callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,17 @@
 const request = require('request');
 
-const requestCatInfo = function(input) {
-  if (input.length !== 3) {
-    console.log('Three arguments needed');
-  }
-  const search = input[2].slice().toLowerCase();
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${search}`, (err, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
     if (err) {
-      console.log('An error occured', err);
-      return;
+      callback('An error occured', null);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log(`${search} not found`);
+      callback(null, `Not found`);
     } else {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
   });
 };
 
-const input = process.argv;
-requestCatInfo(input);
+module.exports = { fetchBreedDescription };
